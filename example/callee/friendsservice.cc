ad56521f3b81779cc8274afd45426638f16dc06f
jyx143866 <1045295202@qq.com>
2025-04-15 11:56:08 +0000

    客户端服务端代码

---
+++ example/callee/friendsservice.cc
@@ -0,0 +1,53 @@
+/*
+ * @Author: jyx
+ * @Date: 2025-04-15 10:02:40
+ * @LastEditors: jyx
+ * @Description:
+ */
+#include <iostream>
+#include <string>
+#include "friend.pb.h"
+#include "mprpcapplication.h"
+#include "rpcprovider.h"
+
+class FriendService : public fixbug::FriendServiceRpc
+{
+public:
+    std::vector<std::string> GetFriendList(uint32_t userid)
+    {
+        std::cout << "do GetFriendList service";
+        std::vector<std::string> vec;
+        vec.push_back("lyh");
+        vec.push_back("jyx");
+        vec.push_back("gjw");
+        return vec;
+    }
+
+    void GetFriendList(google::protobuf::RpcController *controller, const ::fixbug::GetFriendRequest *request, ::fixbug::GetFriendResponse *response, ::google::protobuf::Closure *done)
+    {
+        uint32_t userid = request->userid();
+        std::vector<std::string> friendList = GetFriendList(userid);
+        response->mutable_result()->set_errcode(0);
+        response->mutable_result()->set_errmsg(" ");
+        for(std::string &name : friendList)
+        {
+            std::string *p = response->add_friends();
+            *p = name;
+        }
+        done->Run();
+    }
+};
+
+int main(int argc, char **argv)
+{
+
+    // 调用框架初始化操作
+    MprpcApplication::Init(argc, argv);
+    // provider是一个rpc网络服务对象，把UserService对象发布到rpc节点上
+    RpcProvider provider;
+    provider.NotifyService(new FriendService());
+
+    // 启动一个rpc服务发布节点，run以后，进程进入阻塞状态，等待远程rpc调用请求
+    provider.Run();
+    return 0;
+}
---
+++ example/caller/calluserservice.cc
@@ -0,0 +1,58 @@
+/*
+ * @Author: jyx
+ * @Date: 2025-04-15 05:29:25
+ * @LastEditors: jyx
+ * @Description:
+ */
+#include <iostream>
+#include "mprpcapplication.h"
+#include "user.pb.h"
+#include "mprpcchannel.h"
+#include "friend.pb.h"
+
+int main(int argc, char **argv)
+{
+    // 整个程序启动以后，想使用mprpc框架来享受rpc服务调用，一定要先调用框架的初始化函数（只初始化一次）
+    MprpcApplication::Init(argc, argv);
+    fixbug::UserServiceRpc_Stub stub(new MprpcChannel());
+    fixbug::LoginRequest request;
+    request.set_name("zhang san");
+    request.set_pwd("123456");
+    fixbug::LoginResponse response;
+
+    stub.Login(nullptr, &request, &response, nullptr);
+
+    if(0 == response.result().errcode()){
+        std::cout << "rpc login response success : " << response.success() << std::endl;
+    }
+    else{
+        std::cout << "rpc login response error : " << response.result().errmsg() << std::endl;
+    }
+
+
+    fixbug::RegisterRequest req;
+    req.set_id(2000);
+    req.set_name("jyx");
+    req.set_pwd("123456");
+    fixbug::RegisterResponse rsp;
+
+    stub.Register(nullptr, &req, &rsp, nullptr);
+
+    if (0 == response.result().errcode())
+    {
+        std::cout << "rpc Register response success : " << rsp.success() << std::endl;
+    }
+    else
+    {
+        std::cout << "rpc Register response error : " << rsp.result().errmsg() << std::endl;
+    }
+
+    fixbug::GetFriendRequest greq;
+    greq.set_userid(123);
+    fixbug::GetFriendResponse gres;
+    
+    fixbug::FriendServiceRpc_Stub grpc(new MprpcChannel());
+    grpc.GetFriendList(nullptr, &greq, &gres, nullptr);
+
+    return 0;
+}
