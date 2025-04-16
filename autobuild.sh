296ecffd43db7772c507aef4c278cd2e9c959052
jyx143866 <1045295202@qq.com>
2025-04-16 17:45:55 +0000

    自动化编译脚本

--- autobuild.sh
+++ autobuild.sh
@@ -4,3 +4,14 @@
  # @LastEditors: jyx
  # @Description: 
 ### 
+#! /bin/bash
+set -e
+rm -rf `pwd`/build/*
+mkdir -p bin
+mkdir -p build
+cd `pwd`/build &&
+    cmake .. &&
+    make
+
+cd ..
+cp -r `pwd`/src/include `pwd`/lib
